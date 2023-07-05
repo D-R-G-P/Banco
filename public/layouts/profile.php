df2e1ba788d8914dace442799f01e8ea4216107a
SrCristian00 <crslamas@gmail.com>
2023-07-05 11:27:47 -0300

    init

--- public/layouts/profile.php
+++ public/layouts/profile.php
@@ -75,28 +75,100 @@ $pdo = $db->connect();
   <article>
     <div class="accordion">
       <div class="accordion-item">
+
+
+        <h2 class="accordion-heading">Crear usuario</h2>
+        <div class="accordion-content">
+          <form action="/Banco/app/profile/crear.php" method="post">
+
+            <label for="nombre">Nombres</label>
+            <input type="text" name="nombre" required>
+
+            <label for="apellido">Apellidos</label>
+            <input type="text" name="apellido" required>
+
+            <label for="dni">D.N.I.</label>
+            <input type="text" name="dni" required>
+
+            <label for="username">Nombre de usuario</label>
+            <input type="text" name="username" required>
+
+            <label for="password">Contraseña</label>
+            <input type="text" name="password" value="1234" readonly>
+
+            <label for="cargo">Cargo</label>
+            <input type="text" name="cargo" required>
+
+            <label for="banco">Banco</label>
+            <select name="banco" required>
+              <option value="" selected disabled>Seleccionar una opción</option>
+
+              <?php
+              try {
+                $stmt = $pdo->prepare("SELECT id, banco, siglas FROM bancos");
+                $stmt->execute();
+
+                $options = "";
+                while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+                  $id_banco = $row['id'];
+                  $banco = $row['banco'];
+                  $siglas = $row['siglas'];
+                  $options .= "<option value='$siglas'>$banco - $siglas</option>";
+                }
+
+                // Escribir las opciones en el DOM
+                echo $options;
+              } catch (PDOException $e) {
+                echo 'Error: ' . $e->getMessage();
+              }
+              ?>
+
+              <option value="Otro">Otro</option>
+            </select>
+
+            <label for="tipo_usuario">Tipo de usuario</label>
+            <select name="tipo_usuario" required>
+              <option value="" disabled selected>Seleccionar una opcion</option>
+              <option value="SuperAdmin">SuperAdmin</option>
+              <option value="Admin">Admin</option>
+              <option value="Deposito">Deposito</option>
+              <option value="Instrumentador">Instrumentador</option>
+              <option value="Cirujano">Cirujano</option>
+            </select>
+
+            <button type="submit" class="btn-verde"><i class="fa-solid fa-plus"></i> Crear usuario</button>
+          </form>
+        </div>
+      </div>
+      <div class="accordion-item">
+
+
         <h2 class="accordion-heading">Agregar banco</h2>
         <div class="accordion-content">
           <form action="/Banco/app/profile/banco.php" method="post">
             <label for="banco">Nombre del banco</label>
-            <input type="text" name="banco">
+            <input type="text" name="banco" required>
 
             <label for="sigla">Siglas</label>
-            <input type="text" name="siglas">
+            <input type="text" name="siglas" required>
 
             <button type="submit" class="btn-verde"><i class="fa-solid fa-plus"></i> Agregar banco</button>
           </form>
         </div>
       </div>
       <div class="accordion-item">
+
+
         <h2 class="accordion-heading">Agregar categoria</h2>
         <div class="accordion-content">
-          <form action="/Banco/app/profile/banco.php" method="post">
+          <form action="/Banco/app/profile/categoria.php" method="post">
             <label for="banco">Nombre de la categoria</label>
-            <input type="text" name="banco">
+            <input type="text" name="banco" required>
 
-            <label for="sigla">Banco a la que pertenece</label>
-            <select name="">
+            <label for="sigla">Banco al que pertenece</label>
+            <select name="" required>
+              <option value="" disabled selected>Seleccione una opción</option>
+
               <?php
               try {
                 $stmt = $pdo->prepare("SELECT id, banco, siglas FROM bancos");
@@ -118,13 +190,79 @@ $pdo = $db->connect();
               ?>
             </select>
 
-            <button type="submit" class="btn-verde"><i class="fa-solid fa-plus"></i> Agregar banco</button>
+            <button type="submit" class="btn-verde"><i class="fa-solid fa-plus"></i> Agregar categoria</button>
           </form>
         </div>
       </div>
     </div>
 
+    <div class="datos">
+      <h2 class="datos-cabeza">Modificar mis datos</h2>
+      <div class="datos-contenido">
+        <form action="/Banco/app/profile/modificar.php" method="post">
+
+          <label for="nombre">Nombres</label>
+          <input type="text" name="nombre" required>
+
+          <label for="apellido">Apellidos</label>
+          <input type="text" name="apellido" required>
+
+          <label for="dni">D.N.I.</label>
+          <input type="text" name="dni" required>
+
+          <label for="username">Nombre de usuario</label>
+          <input type="text" name="username" required disabled>
+
+          <label for="password">Contraseña</label>
+          <input type="password" name="password" value="1234" readonly>
+
+          <label for="cargo">Cargo</label>
+          <input type="text" name="cargo" required disabled>
+
+          <label for="banco">Banco</label>
+          <select name="banco" required disabled>
+            <option value="" selected disabled>Seleccionar una opción</option>
+
+            <?php
+            try {
+              $stmt = $pdo->prepare("SELECT id, banco, siglas FROM bancos");
+              $stmt->execute();
+
+              $options = "";
+              while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+                $id_banco = $row['id'];
+                $banco = $row['banco'];
+                $siglas = $row['siglas'];
+                $options .= "<option value='$siglas'>$banco - $siglas</option>";
+              }
+
+              // Escribir las opciones en el DOM
+              echo $options;
+            } catch (PDOException $e) {
+              echo 'Error: ' . $e->getMessage();
+            }
+            ?>
+
+            <option value="Otro">Otro</option>
+          </select>
+
+          <label for="tipo_usuario">Tipo de usuario</label>
+          <select name="tipo_usuario" required disabled>
+            <option value="" disabled selected>Seleccionar una opcion</option>
+            <option value="SuperAdmin">SuperAdmin</option>
+            <option value="Admin">Admin</option>
+            <option value="Deposito">Deposito</option>
+            <option value="Instrumentador">Instrumentador</option>
+            <option value="Cirujano">Cirujano</option>
+          </select>
+
+          <button type="submit" class="btn-verde"><i class="fa-solid fa-plus"></i> Modificar datos</button>
+        </form>
+      </div>
+    </div>
+
   </article>
+
 
   <footer>
     &copy; Dirección de Redes y Gestión de Personas. Todos los derechos reservados
